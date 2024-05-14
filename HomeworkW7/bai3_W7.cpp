53ab5b92a491e97ea4593470133a8bb47ebd864d
Tran Huy <160874231+tranhuy2206@users.noreply.github.com>
2024-05-14 22:47:18 +0700

    Update bai3_W7.cpp

--- HomeworkW7/bai3_W7.cpp
+++ HomeworkW7/bai3_W7.cpp
@@ -3,9 +3,21 @@ using namespace std;
 
 
 
-int main()
-{
-    double x = 3.0/5.0;
-    cout << x;
+int power(int a, int b){
+    int res = 1;
+    for (int i = 1; i <= b; i++){
+        res *= a;
+    }
+    return res;
+}
 
+int powersum1(int x, int n, int k){
+    int smallest_power = power(k,n);
+    if (smallest_power > x) return 0;
+    if (smallest_power == x) return 1;
+    return (powersum1(x-smallest_power,n,k+1)+powersum1(x,n,k+1));
+}
+
+int powerSum(int X, int N) {
+    return powersum1(X,N,1);
 }
